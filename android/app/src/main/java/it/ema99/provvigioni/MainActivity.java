199ec7194cb3a056249b3ed2e8d1ac9aa2986e1f
Manu99it <emanuele.luciani.99@gmail.com>
2026-05-24 18:03:47 +0200

    Match Android WebView startup background

--- android/app/src/main/java/it/ema99/provvigioni/MainActivity.java
+++ android/app/src/main/java/it/ema99/provvigioni/MainActivity.java
@@ -2,14 +2,24 @@ package it.ema99.provvigioni;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.view.Window;
 import android.view.WindowManager;
 import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private int getLaunchBackgroundColor() {
+        int nightModeFlags = getResources().getConfiguration().uiMode & android.content.res.Configuration.UI_MODE_NIGHT_MASK;
+        if (nightModeFlags == android.content.res.Configuration.UI_MODE_NIGHT_YES) {
+            return Color.rgb(8, 47, 73);
+        }
+        return Color.rgb(240, 249, 255);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(getLaunchBackgroundColor()));
         super.onCreate(savedInstanceState);
 
         Window window = getWindow();
@@ -26,8 +36,10 @@ public class MainActivity extends BridgeActivity {
     public void onStart() {
         super.onStart();
         if (bridge != null && bridge.getWebView() != null) {
-            bridge.getWebView().setVerticalScrollBarEnabled(false);
-            bridge.getWebView().setHorizontalScrollBarEnabled(false);
+            WebView webView = bridge.getWebView();
+            webView.setBackgroundColor(getLaunchBackgroundColor());
+            webView.setVerticalScrollBarEnabled(false);
+            webView.setHorizontalScrollBarEnabled(false);
         }
     }
 }
